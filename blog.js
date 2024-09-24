304af0caa452db3a6f81aacf784a9b0d4b991bd2
Abubokkor98 <mail.abubokkor@gmail.com>
2024-09-24 08:48:35 +0600

    blog html file added

--- blog.js
+++
@@ -1,6 +0,0 @@
-console.log('test connected');
-const homePage = document.getElementById('home');
-homePage.addEventListener('click',function(){
-    console.log('home btn clicked');
-    window.location.href = './index.html';
-})
